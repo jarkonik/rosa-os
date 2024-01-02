637b93da44aa3473a9ef1e1f65ce919636f38da7
Jaroslaw Konik <konikjar@gmail.com>
2024-01-02 18:04:12 +0100

    cleanup

--- kernel/src/interrupts.rs
+++ kernel/src/interrupts.rs
@@ -5,7 +5,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
-        idt.double_fault.set_handler_fn(double_fault_handler); // new
+        idt.double_fault.set_handler_fn(double_fault_handler);
         idt
     };
 }
@@ -18,7 +18,6 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     log::info!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
 
-// new
 extern "x86-interrupt" fn double_fault_handler(
     stack_frame: InterruptStackFrame,
     _error_code: u64,
